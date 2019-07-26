eb080b2955bd8bc61693e4fb4c3abb638c732de5
unknown <walavalkaronkar@gmail.com>
2019-07-26 09:47:51 +0530

    ionic proj7

--- ionicnew/BookMyFair/src/app/component/register/register.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
-
-
-export class RegisterModule {}
